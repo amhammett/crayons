261e88e43025d8a7ebfb29d04860e4a93bae727c
amhammett <am.hammett@gmail.com>
2020-01-09 12:41:05 +1100

    py unittests with basic coverage

---
+++ tests/test_crayon.py
@@ -0,0 +1,10 @@
+import unittest
+
+from src import crayon
+
+class TestTrue(unittest.TestCase):
+    def test_import(self):
+        self.assertTrue(crayon)
+
+    def test_true(self):
+        self.assertTrue(True)
